607d68718c3d2f3bf5de73ffb243de31ac7cf6b2
Teddy Roncin <teddy.roncin@proton.me>
2025-11-09 15:14:57 +0100

    Texture for car

--- src/gui.rs
+++ src/gui.rs
@@ -1,23 +1,26 @@
-use macroquad::prelude::*;
 use crate::road;
+use macroquad::{color::*};
 
 const WINDOW_MARGINS: f32 = 10.;
+const CAR_SIZE: (f32, f32) = (28.8, 14.28);
 
 pub struct Window {
     width: f32,
     height: f32,
+    car_texture: macroquad::texture::Texture2D,
 }
 
 impl Window {
-    pub fn new() -> Self {
+    pub async fn new() -> Self {
         Self {
-            width: screen_width() - WINDOW_MARGINS * 2.,
-            height: screen_height() - WINDOW_MARGINS * 2.,
+            width: macroquad::window::screen_width() - WINDOW_MARGINS * 2.,
+            height: macroquad::window::screen_height() - WINDOW_MARGINS * 2.,
+            car_texture: macroquad::texture::load_texture("resources/textures/car.png").await.expect("Could not load car texture"),
         }
     }
 
     pub fn draw_road_segment(&self, start: (f32, f32), end: (f32, f32)) {
-        draw_line(self.x_to_pixel(start.0), self.y_to_pixel(start.1), self.x_to_pixel(end.0), self.y_to_pixel(end.1), 3., RED);
+        macroquad::shapes::draw_line(self.x_to_pixel(start.0), self.y_to_pixel(start.1), self.x_to_pixel(end.0), self.y_to_pixel(end.1), 3., RED);
     }
 
     pub fn draw_sign(&self, (x, y): (f32, f32), sign_type: &road::SignType) {
@@ -25,13 +28,20 @@ impl Window {
             road::SignType::SpeedLimit => { ORANGE }
             road::SignType::EndSpeedLimit => { BLUE }
         };
-        draw_rectangle(self.x_to_pixel(x) - 5., self.y_to_pixel(y) - 5., 10., 10., color);
+        macroquad::shapes::draw_rectangle(self.x_to_pixel(x) - 5., self.y_to_pixel(y) - 5., 10., 10., color);
     }
 
     pub fn draw_car(&self, (x, y): (f32, f32), speed: f32) {
-        draw_rectangle(self.x_to_pixel(x) - 5., self.y_to_pixel(y) - 5., 10., 10., BLUE);
+        // macroquad::shapes::draw_rectangle(self.x_to_pixel(x) - CAR_SIZE.0 / 2., self.y_to_pixel(y) - CAR_SIZE.1 / 2., CAR_SIZE.0, CAR_SIZE.1, ORANGE);
+        macroquad::texture::draw_texture_ex(
+            &self.car_texture,
+            self.x_to_pixel(x) - CAR_SIZE.0 / 2.,
+            self.y_to_pixel(y) - CAR_SIZE.1 / 2.,
+            WHITE,
+            macroquad::texture::DrawTextureParams { dest_size: Some(macroquad::math::Vec2::new(CAR_SIZE.0, CAR_SIZE.1)), ..Default::default() }
+        );
         if speed != -1. {
-            draw_text(&("Speed = ".to_string() + &speed.to_string()), 5., 100., 30., WHITE);
+            macroquad::text::draw_text(&("Speed = ".to_string() + &speed.to_string()), 5., 100., 30., WHITE);
         }
     }
 
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ mod road;
 mod agent;
 mod gui;
 
-use macroquad::{prelude::*};
-
 struct SimulationData {
     roads: road::Roads,
     cars: Vec<agent::car::Car>,
@@ -13,7 +11,7 @@ struct SimulationData {
 #[macroquad::main("MyGame")]
 async fn main() {
     let mut sd: SimulationData = SimulationData { roads: road::Roads::new(), cars: Vec::new() };
-    let window = gui::Window::new();
+    let window = gui::Window::new().await;
     sd.cars.push(agent::car::Car::new(road::RoadPoint::new(0, 0.)));
     // for _ in 0..10 {
     //     let road_idx: usize = gen_range(0, sd.roads.segments.len());
@@ -21,17 +19,17 @@ async fn main() {
     //     sd.cars.push(Car::new(roads::RoadPoint::new(road_idx, progression)));
     // }
     loop {
-        let step_size = get_frame_time();
+        let step_size = macroquad::prelude::get_frame_time();
         let roads = &sd.roads;
         for car in sd.cars.iter_mut() {
             car.step(step_size, roads);
         }
-        clear_background(BLACK);
+        macroquad::window::clear_background(macroquad::color::BLACK);
         sd.roads.render(&window);
         for (i, car) in sd.cars.iter().enumerate() {
             car.render(&window, &sd.roads, i == 0)
         }
-        draw_fps();
-        next_frame().await;
+        macroquad::time::draw_fps();
+        macroquad::window::next_frame().await;
     }
 }
